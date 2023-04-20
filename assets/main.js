259fe7955e1655280474378bbe3e828f28bd0b2a
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-20 16:42:14 +0300

    first commit

---
+++ assets/main.js
@@ -0,0 +1,29 @@
+const socket = io()
+const messages = document.querySelector('.messages')
+const form = document.querySelector('.form')
+const input = document.querySelector('.input')
+const nameBlock = document.querySelector('.name')
+
+const userName = prompt('Your name:')
+nameBlock.innerHTML = `${userName}`
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	if (input.value) {
+		socket.emit('chat message', {
+			message: input.value,
+			name: userName,
+		})
+		input.value = ''
+	}
+})
+
+socket.on('chat message', (data) => {
+	const item = document.createElement('li')
+	item.innerHTML = `<span>${data.name}</span>: ${data.message}`
+
+	messages.prepend(item)
+	input.focus()
+	window.scrollTo(0, document.body.scrollHeight)
+})
